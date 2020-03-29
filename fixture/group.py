74646b72a439c9b77a70fe02f5357b0793dd5ce3
e.voronova <voronova.eal@gmail.com>
2020-03-29 19:23:40 +0700

    Добавлен тест на удаление первой группы из списка. Фикстура вынесена в отдельный файл и стала общей для всех тестов

--- fixture/group.py
+++ fixture/group.py
@@ -26,6 +26,15 @@ class GroupHelper:
         # return to group page
         self.return_to_group_page()
 
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+        self.return_to_group_page()
+
     def return_to_group_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("group page").click()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,20 +1,12 @@
 # -*- coding: utf-8 -*-
-import pytest
 from model.group import Group
-from fixture.application import Application
-
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 
 def test_group_add(app):
     app.session.login(username="admin", password="secret")
     app.group.create(Group(name="test group", header="test", footer="test"))
     app.session.logout()
+
 
 
 def test_empty_group_add(app):

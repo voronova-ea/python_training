4f052534bf8ba5220c5dd9961ce31da7aa453b50
e.voronova <voronova.eal@gmail.com>
2020-03-30 16:47:26 +0700

    Заполнение форм групп и контактов вынесены в отдельные методы, вызываемые при их создании и редактировании

--- fixture/group.py
+++ fixture/group.py
@@ -6,12 +6,8 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
 
-    def create(self, group):
+    def fill_form(self, group):
         wd = self.app.wd
-        self.open_groups_page()
-        # init group creation
-        wd.find_element_by_name("new").click()
-        # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
@@ -21,6 +17,13 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
+
+    def create(self, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        # init group creation
+        wd.find_element_by_name("new").click()
+        self.fill_form(group)
         # submit group creation
         wd.find_element_by_name("submit").click()
         # return to group page
@@ -33,16 +36,7 @@ class GroupHelper:
         wd.find_element_by_name("selected[]").click()
         # init group editing
         wd.find_element_by_name("edit").click()
-        # edit group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_form(group)
         # submit editing
         wd.find_element_by_name("update").click()
         self.return_to_group_page()

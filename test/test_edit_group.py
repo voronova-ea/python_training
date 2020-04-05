8a25f482c4c2ea83b18f685af696b3c201204a38
e.voronova <voronova.eal@gmail.com>
2020-04-05 13:43:23 +0700

    Добавлены проверка и обеспечение предусловий в тесты на удаление и редактирование групп

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -2,20 +2,30 @@ from model.group import Group
 
 
 def test_group_edit_all_fields(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="name", header="header", footer="footer"))
     app.group.edit_first_group(Group(name="new name", header="new header", footer="new footer"))
 
 
 def test_group_edit_all_fields_empty(app):
-    app.group.edit_first_group(Group(name="", header="", footer="1"))
+    if app.group.count() == 0:
+        app.group.create(Group(name="name", header="header", footer="footer"))
+    app.group.edit_first_group(Group(name="", header="", footer=""))
 
 
 def test_group_edit_name(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.edit_first_group(Group(name="new name"))
 
 
 def test_group_edit_header(app):
+    if app.group.count() == 0:
+        app.group.create(Group(header=""))
     app.group.edit_first_group(Group(header="new header"))
 
 
 def test_group_edit_footer(app):
+    if app.group.count() == 0:
+        app.group.create(Group(footer="test"))
     app.group.edit_first_group(Group(footer="new footer"))

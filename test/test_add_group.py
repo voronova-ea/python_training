65d9a125015bbdffa769627b5272bcc1913bfcba
e.voronova <voronova.eal@gmail.com>
2020-04-11 17:21:48 +0700

    В фикстуру добавлен метод получения списка групп и в тесты добавлены проверки на корректное количество групп в списке после их создания, удаления и редактирования.

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,8 +3,14 @@ from model.group import Group
 
 
 def test_group_add(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="test group", header="test", footer="test"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 
 def test_empty_group_add(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -3,24 +3,39 @@ from model.group import Group
 
 def test_group_edit_all_fields(app):
     app.group.check(Group(name="name", header="header", footer="footer"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(name="new name", header="new header", footer="new footer"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_group_edit_all_fields_empty(app):
     app.group.check(Group(name="name", header="header", footer="footer"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_group_edit_name(app):
     app.group.check(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(name="new name"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_group_edit_header(app):
     app.group.check(Group(header=""))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(header="new header"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_group_edit_footer(app):
     app.group.check(Group(footer="test"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(footer="new footer"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)

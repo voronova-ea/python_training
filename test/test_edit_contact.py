b61b3250657252f72bb79173718704c484adcca4
e.voronova <voronova.eal@gmail.com>
2020-05-02 17:32:47 +0700

    Добавлены метод загрузки списка групп из базы данных и тест на соответствие данных этого списка списку групп в UI.

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -14,5 +14,5 @@ def test_contact_edit_all_fields(app, json_contacts):
     app.contact.edit_contact_by_index(index, contact)
     assert len(old_contacts) == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    old_contacts[index] = contact
+    old_contacts[index] = app.contact.clean_spaces(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)

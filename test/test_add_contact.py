6f32ba0e6e976bca320624c845eed1bc907a620a
e.voronova <voronova.eal@gmail.com>
2020-05-24 21:03:55 +0700

    Реализована генерация отчётов о выполнении тестов на создание групп и контактов с использование генератора отчетов Allure.

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import allure
 
 
 def test_contact_add(app, orm, json_contacts, check_ui):
     contact = json_contacts
-    old_contacts = orm.get_contact_list()
-    app.contact.create(contact)
-    new_contacts = orm.get_contact_list()
-    old_contacts.append(contact)
-    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-    # опциональная проверка, если при запуске указан check_ui
-    app.contact.ui_check(orm, check_ui)
+    with allure.step('Given a contact list'):
+        old_contacts = orm.get_contact_list()
+    with allure.step('When I add a contact %s to the list' % contact):
+        app.contact.create(contact)
+    with allure.step('Then the new contact list is equal to the old list with the added contact'):
+        new_contacts = orm.get_contact_list()
+        old_contacts.append(contact)
+        assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+        # опциональная проверка, если при запуске указан check_ui
+        app.contact.ui_check(orm, check_ui)
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+import allure
 
 
 def test_group_add(app, db, json_groups, check_ui):
     group = json_groups
-    old_groups = db.get_group_list()
-    app.group.create(group)
-    new_groups = db.get_group_list()
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-    # опциональная проверка, если при запуске указан check_ui
-    app.group.ui_check(db, check_ui)
+    with allure.step('Given a group list'):
+        old_groups = db.get_group_list()
+    with allure.step('When I add a group %s to the list' % group):
+        app.group.create(group)
+    with allure.step('Then the new group list is equal to the old list with the added group'):
+        new_groups = db.get_group_list()
+        old_groups.append(group)
+        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+        # опциональная проверка, если при запуске указан check_ui
+        app.group.ui_check(db, check_ui)
